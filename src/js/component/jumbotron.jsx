2fd749679956ce7f0de80987eda4cd29181ac78e
labs404 <136141267+labs404@users.noreply.github.com>
2023-07-25 05:35:32 +0000

    typo

--- src/js/component/jumbotron.jsx
+++ src/js/component/jumbotron.jsx
@@ -5,7 +5,7 @@ const Jumbotron = () => {
         <div className="p-4 bg-secondary-subtle border border-dark rounded-3 mt-5" id="jumbotron">
             <h5>NINE INCH NAILS: Major Releases</h5>
             <p>Nine Inch Nails front-man Trent Reznor has a vast and varied portfolio of music ranging from full cinematic scores, to collaboration projects, to classic record releases. Below is a chronological snapshot of his main record releases with Nine Inch Nails.</p>
-            <a className="btn btn-outline-danger" type="button" href="https://en.wikipedia.org/wiki/Trent_Reznor" target="_blank">Trent Reznor Wiki</a> <a className="btn btn-outline-danger" type="button" href="https://en.wikipedia.org/wiki/Nine_Inch_Nails" target="_blank">Nine Inch Naile Wiki</a>
+            <a className="btn btn-outline-danger" type="button" href="https://en.wikipedia.org/wiki/Trent_Reznor" target="_blank">Trent Reznor Wiki</a> <a className="btn btn-outline-danger" type="button" href="https://en.wikipedia.org/wiki/Nine_Inch_Nails" target="_blank">Nine Inch Nails Wiki</a>
         </div>
     );
 }
